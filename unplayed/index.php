3ac48c4f317c3aa529b20c1a287540b029bafa80
Kyle O'Brien <me@kyleobrien.net>
2025-04-07 00:00:37 -0400

    Clean up formatting and refactor parsing of markdown into html.

--- unplayed/index.php
+++ unplayed/index.php
@@ -5,179 +5,161 @@ require_once 'libs/Michelf/SmartyPants.inc.php';
 
 use Michelf\Markdown, Michelf\SmartyPants;
 
-function e($str) { echo preg_replace('#\(([^\)]+)\)#', '<span>\1</span>', $str); }
+function e($html) { echo preg_replace('#\(([^\)]+)\)#', '<span>\1</span>', $html); }
+function t($filename) {
+	$text = file_get_contents($filename);
+	$html = Markdown::defaultTransform($text);
+	$html = SmartyPants::defaultTransform($html);
+
+	return $html;
+}
 
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Unplayed</title>
-<meta name="viewport" content="width=320, initial-scale=1, minimum-scale=0.45" />
-<style>
+<html lang="en-US">
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width" />
+		<!--<meta name="viewport" content="width=320, initial-scale=1, minimum-scale=0.45" />-->
+		<title>Unplayed</title>
+		<style>
 
-*
-{
-	margin: 0;
-	padding: 0;
-	border: none;
-}
+		*
+		{
+			margin: 0;
+			padding: 0;
+			border: none;
+		}
 
-body
-{
-	font: 13px/18px sans-serif;
-	color: white;
-	background-color: #1b1b1e;
-	padding: 16px;
-}
+		body
+		{
+			font: 13px/18px sans-serif;
+			color: white;
+			background-color: #1b1b1e;
+			padding: 16px;
+		}
 
-a
-{
-	text-decoration: none;
-	font-weight: bold;
-	color: inherit;
-}
+		a
+		{
+			text-decoration: none;
+			font-weight: bold;
+			color: inherit;
+		}
 
-a:hover
-{
-	color: #333;
-}
+		a:hover
+		{
+			color: #333;
+		}
 
-div
-{
-	-webkit-box-sizing: border-box;
-	-ms-box-sizing: border-box;
-	-moz-box-sizing: border-box;
-	-o-box-sizing: border-box;
-	box-sizing: border-box;
-	padding: 16px;
-	width: 25%;
-	float: left;
-}
+		div
+		{
+			-webkit-box-sizing: border-box;
+			-ms-box-sizing: border-box;
+			-moz-box-sizing: border-box;
+			-o-box-sizing: border-box;
+			box-sizing: border-box;
+			padding: 16px;
+			width: 25%;
+			float: left;
+		}
 
-#footer
-{
-	margin: 16px 16px 0;
-	padding: 16px 0 0;
-	border-top: 1px solid #333;
-	float: none;
-	clear: both;
-	width: auto;
-}
+		#footer
+		{
+			margin: 16px 16px 0;
+			padding: 16px 0 0;
+			border-top: 1px solid #333;
+			float: none;
+			clear: both;
+			width: auto;
+		}
 
-h1
-{
-	margin: 0 0 16px;
-	padding: 0 0 16px;
-	border-bottom: 4px solid #333;
-}
+		h1
+		{
+			margin: 0 0 16px;
+			padding: 0 0 16px;
+			border-bottom: 4px solid #333;
+		}
 
-p
-{
-	margin: 0 0 16px;
-}
+		p
+		{
+			margin: 0 0 16px;
+		}
 
-li
-{
-	list-style: none;
-	padding: 0 0 0 16px;
-	position: relative;
-}
+		li
+		{
+			list-style: none;
+			padding: 0 0 0 16px;
+			position: relative;
+		}
 
-li:before
-{
-	display: block;
-	content: '-';
-	font-weight: bold;
-	position: absolute;
-	top: -2px;
-	left: 0;
-	font-size: 24px;
-}
+		li:before
+		{
+			display: block;
+			content: '-';
+			font-weight: bold;
+			position: absolute;
+			top: -2px;
+			left: 0;
+			font-size: 24px;
+		}
 
-span
-{
-	color: #999;
-	font-size: 9px;
-}
+		span
+		{
+			color: #999;
+			font-size: 9px;
+		}
 
-span + span
-{
-	font-size: 11px;
-	line-height: 13px;
-	display: block;
-	position: relative;
-	top: -2px;
-}
+		span + span
+		{
+			font-size: 11px;
+			line-height: 13px;
+			display: block;
+			position: relative;
+			top: -2px;
+		}
 
-/* iPhone [portrait + landscape] */
-@media only screen and (max-device-width: 480px)
-{
-	body
-	{
-		padding: 0;
-	}
-	div
-	{
-		width: 320px;
-		clear: both;
-	}
-	#footer
-	{
-		margin: 16px;
-	}
-}
+		/* iPhone [portrait + landscape] */
+		@media only screen and (max-device-width: 480px)
+		{
+			body
+			{
+				padding: 0;
+			}
+			div
+			{
+				width: 320px;
+				clear: both;
+			}
+			#footer
+			{
+				margin: 16px;
+			}
+		}
+		</style>
+	</head>
+	<body>
+	<div>
+	<h1>Unplayed</h1>
+	<?php e(t('unplayed.markdown')); ?>
+	</div>
 
-</style>
-</head>
-<body>
+	<div>
+	<h1>Unbeaten</h1>
+	<?php e(t('unbeaten.markdown')); ?>
+	</div>
 
-<div>
-<h1>Unplayed</h1>
-<?php 
+	<div>
+	<h1>Beaten</h1>
+	<?php e(t('beaten.markdown')); ?>
+	</div>
 
-$unplayed_text = file_get_contents('unplayed.markdown');
-$unplayed_html = Markdown::defaultTransform($unplayed_text);
-$unplayed_html = SmartyPants::defaultTransform($unplayed_html);
-e($unplayed_html);
+	<div>
+	<h1>Abandoned</h1>
+	<?php e(t('abandoned.markdown')); ?>
+	</div>
 
-?>
-</div>
+	<div id="footer"><a href="http://shauninman.com/">Shaun Inman</a> gave me this idea. <a href="http://shauninman.com/archive/2011/04/18/unplayed">More info</a></div>
 
-<div>
-<h1>Unbeaten</h1>
-<?php 
-
-$unbeaten_text = file_get_contents('unbeaten.markdown');
-$unbeaten_html = Markdown::defaultTransform($unbeaten_text);
-$unbeaten_html = SmartyPants::defaultTransform($unbeaten_html);
-e($unbeaten_html);
-
-?>
-</div>
-
-<div>
-<h1>Beaten</h1>
-<?php
-$beaten_text = file_get_contents('beaten.markdown');
-$beaten_html = Markdown::defaultTransform($beaten_text);
-$beaten_html = SmartyPants::defaultTransform($beaten_html);
-e($beaten_html);
-?>
-</div>
-
-<div>
-<h1>Abandoned</h1>
-<?php
-$abandoned_text = file_get_contents('abandoned.markdown');
-$abandoned_html = Markdown::defaultTransform($abandoned_text);
-$abandoned_html = SmartyPants::defaultTransform($abandoned_html);
-e($abandoned_html);
-?>
-</div>
-
-<div id="footer"><a href="http://shauninman.com/">Shaun Inman</a> gave me this idea. <a href="http://shauninman.com/archive/2011/04/18/unplayed">More info</a></div>
-
-</body>
+	</body>
 </html>
